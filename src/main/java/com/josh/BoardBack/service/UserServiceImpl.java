9572aa8cdf6496d074946ff8f85cb49480568804
josh <tkddns6007@gmail.com>
2023-03-12 17:44:29 +0900

    Controller 생성 및 MySQL 서버연결 CommandLine으로 사용자, 권한 데이터 넣기

--- src/main/java/com/josh/BoardBack/service/UserServiceImpl.java
+++ src/main/java/com/josh/BoardBack/service/UserServiceImpl.java
@@ -4,14 +4,17 @@ import com.josh.BoardBack.domain.Role;
 import com.josh.BoardBack.domain.User;
 import com.josh.BoardBack.repo.RoleRepo;
 import com.josh.BoardBack.repo.UserRepo;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class UserServiceImpl implements UserService{
 
     private final UserRepo userRepo;
@@ -19,27 +22,34 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
-        return null;
+        log.info("saving new {} user database",user.getName());
+        return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        log.info("saving new role {} database", role.getName());
+        return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
-
+        log.info("Adding role {} to user {} database", roleName, username);
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        log.info("Fetching user {}", username);
+        return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        log.info("Fetching all users");
+        return userRepo.findAll();
     }
 }
 
