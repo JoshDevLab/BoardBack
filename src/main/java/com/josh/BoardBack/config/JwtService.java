d0697b38ea0ac0c871633c67554a70fd8b48050b
josh <tkddns6007@gmail.com>
2023-03-15 00:05:01 +0900

    Jwt Filter, JwtService Class Create

---
+++ src/main/java/com/josh/BoardBack/config/JwtService.java
@@ -0,0 +1,42 @@
+package com.josh.BoardBack.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private static final String SECRET_KEY = "5368566D597133743677397A24432646294A404D635166546A576E5A72347537";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] ketBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(ketBytes);
+    }
+
+}
